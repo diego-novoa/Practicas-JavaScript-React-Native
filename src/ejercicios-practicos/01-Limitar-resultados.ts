df9c802506ed634a8f2a86a47ee2d8f473442d42
Diego Novoa <diegonovoa1981@hotmail.com>
2026-03-05 09:56:00 -0500

    meojras en tarea

--- src/ejercicios-practicos/01-Limitar-resultados.ts
+++ src/ejercicios-practicos/01-Limitar-resultados.ts
@@ -3,14 +3,42 @@
 2. Crea `const firstFive = users.slice(0, 5)`.
 3. Llama `renderUsers(firstFive)`.*/
 
-import { renderUsers } from './01-Limitar-resultados';
+import { renderUsers } from './01-Limitar-resultdos';
 
 const loadUsers = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const users = await response.json();
+	// Hace una petición a la API y espera la respuesta.
+	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	// Convierte la respuesta a JSON y obtiene todos los usuarios.
+	const user = await response.json()
+	// Toma solo los primeros 5 usuarios del arreglo.
+	const firstFive = user.slice(0, 5);
+	// Envíar esos 5 usuarios a renderUsers para mostrarlos.
+	renderUsers(firstFive);
+}
+// Ejecutar la función.
+loadUsers();
 
-    const firstFive = users.slice(0, 5);
-    renderUsers(firstFive);
-};
 
+//2-  segunda version Creando un tipo 
+
+// Creamos un tipo
+type user = {
+
+	id: number;
+	name: string;
+	username: string;
+	string: string;
+}
+
+const loadUsers = async (): Promise<void> => {
+	// Hace una petición a la API y espera la respuesta.
+	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	// Convierte la respuesta a JSON y obtiene todos los usuarios.
+	const user = await response.json();
+	// Toma solo los primeros 5 usuarios del arreglo.
+	const firstFive = user.slice(0, 5);
+	// Envíar esos 5 usuarios a renderUsers para mostrarlos.
+	renderUsers(firstFive);
+}
+// Ejecutar la función.
 loadUsers();
