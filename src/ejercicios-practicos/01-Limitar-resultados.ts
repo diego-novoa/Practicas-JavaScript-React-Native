add7458dd91658e616071306726689cda01086fe
Diego Novoa <diegonovoa1981@hotmail.com>
2026-03-05 08:47:30 -0500

    desarrollo ejercicio

--- src/ejercicios-practicos/01-Limitar-resultados.ts
+++ src/ejercicios-practicos/01-Limitar-resultados.ts
@@ -2,3 +2,15 @@
 1. En `loadUsers`, guarda la data completa en `const users`.
 2. Crea `const firstFive = users.slice(0, 5)`.
 3. Llama `renderUsers(firstFive)`.*/
+
+import { renderUsers } from './01-Limitar-resultados';
+
+const loadUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+
+    const firstFive = users.slice(0, 5);
+    renderUsers(firstFive);
+};
+
+loadUsers();
--- src/ejercicios-practicos/03-OrdenarA-Z y Z-A.ts
+++ src/ejercicios-practicos/03-OrdenarA-Z y Z-A.ts
@@ -1,4 +1,8 @@
-/* Ejercicio 3: Ordenar A-Z y Z-A
+/* 
+leventar un proyecto react, y consumir una api
+
+
+Ejercicio 3: Ordenar A-Z y Z-A (construir filtro de busqueda)(input de tipo text)
 1. Agrega dos botones: "Ordenar A-Z" y "Ordenar Z-A".
 2. Clona el arreglo con `[...users]`.
 3. Ordena con `localeCompare`.*/
